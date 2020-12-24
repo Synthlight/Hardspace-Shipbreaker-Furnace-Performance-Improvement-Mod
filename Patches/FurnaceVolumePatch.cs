c2e59d508eef79262808f7c4f178d8f12b9f90b4
Gregory Brutsche <gregory@mail.brutsches.local>
2020-12-24 11:43:04 -0500

    Initial commit.

---
+++ Patches/FurnaceVolumePatch.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Reflection;
+using BBI.Unity.Game;
+using HarmonyLib;
+using JetBrains.Annotations;
+using Unity.Entities;
+
+namespace FurnacePerformanceImprovements.Patches {
+    [HarmonyPatch]
+    [UsedImplicitly]
+    public class FurnaceVolumePatch1 {
+        [HarmonyTargetMethod]
+        [UsedImplicitly]
+        public static MethodBase TargetMethod() {
+            return typeof(FurnaceVolume).GetMethod("HandlePositiveSalvage", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        [HarmonyPrefix]
+        [UsedImplicitly]
+        public static bool Prefix(ref FurnaceVolume __instance, ref EntityCommandBuffer commandBuffer, ref EntityManager entityManager, ref Entity entity, ref StructurePart part) {
+            InvokeDestroy(__instance, entityManager, entity, part);
+            SalvageableUtility.MarkAsSalvaged(commandBuffer, entityManager, entity, false, SalvagedBy.Furnace);
+            return false;
+        }
+
+        private static MethodInfo destroyPartMethod;
+
+        public static void InvokeDestroy(FurnaceVolume __instance, EntityManager entityManager, Entity entity, StructurePart part) {
+            if (destroyPartMethod == null) {
+                destroyPartMethod = typeof(FurnaceVolume).GetMethod("DestroyPart", BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            }
+
+            var destroyPartResult = (IEnumerator) destroyPartMethod.Invoke(__instance, new object[] {Main.Instance.MainSettings.SalvageSettings.ObjectDestructionDelay, part, entityManager, entity});
+            __instance.StartCoroutine(destroyPartResult);
+        }
+    }
+
+    [HarmonyPatch]
+    [UsedImplicitly]
+    public class FurnaceVolumePatch2 {
+        [HarmonyTargetMethod]
+        [UsedImplicitly]
+        public static MethodBase TargetMethod() {
+            return typeof(FurnaceVolume).GetMethod("HandleNegativeSalvage", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        [HarmonyPrefix]
+        [UsedImplicitly]
+        public static bool Prefix(ref FurnaceVolume __instance, ref EntityCommandBuffer commandBuffer, ref EntityManager entityManager, ref Entity entity, ref StructurePart part) {
+            FurnaceVolumePatch1.InvokeDestroy(__instance, entityManager, entity, part);
+            SalvageableUtility.MarkAsDestroyed(commandBuffer, entityManager, entity, SalvagedBy.Furnace);
+            return false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+
+namespace FurnacePerformanceImprovements {
+    [BepInPlugin(UUID, "Furnace Performance Improvements", "1.0.0.0")]
+    [UsedImplicitly]
+    public class Plugin : BaseUnityPlugin {
+        private const  string          UUID = "com.FurnacePerformanceImprovements";
+        private static ManualLogSource logSource;
+
+        [UsedImplicitly]
+        public void Awake() {
+            logSource = Logger;
+
+            Log(LogLevel.Info, "FurnacePerformanceImprovements loaded.");
+
+            var harmony = new Harmony(UUID);
+            harmony.PatchAll();
+
+            Log(LogLevel.Info, "FurnacePerformanceImprovements patched.");
+
+            foreach (var patchedMethod in harmony.GetPatchedMethods()) {
+                Log(LogLevel.Info, $"Patched: {patchedMethod.DeclaringType?.FullName}:{patchedMethod}");
+            }
+        }
+
+        public static void Log(LogLevel level, string msg) {
+            logSource.Log(level, msg);
+        }
+    }
+}
